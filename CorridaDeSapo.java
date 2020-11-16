43087569bea79fc0b38c1c3635a07194a27668fc
gcasq <gustavoacasqueiro@gmail.com>
2020-11-16 11:06:57 -0300

    Adicionadas as funções de salto dentro da classe sapos

--- CorridaDeSapo.java
+++ CorridaDeSapo.java
@@ -1,12 +1,24 @@
+import java.lang.Math;
+
 class sapos extends Thread{
     String nome;
-    int distpercurso,distpercorrida,numsaltos;
+    int distpercurso,distpercorrida,numsaltos,saltomax;
     sapos(String nome,int distpercurso){
         this.nome = nome;
         this.distpercurso = distpercurso;
         this.distpercorrida = 0;
         this.numsaltos = 0;
+        this.saltomax = 30;
     }
+    void show(int aux){
+        System.out.println("O "+nome+" pulou "+aux+" casas    Percorreu "+distpercorrida);
+    }
+    void jump(){
+        int aux = (int) Math.random()*saltomax;
+        distpercorrida+=aux;
+        show(aux);
+    }
+    
 
 }
 

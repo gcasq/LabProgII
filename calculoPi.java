4f0f5591ba3f748ff6eb19460ca0b300bd767893
gcasq <gustavoacasqueiro@gmail.com>
2020-11-16 12:13:08 -0300

    adicionando a função de incrementar o valor atual de Pi

--- calculoPi.java
+++ calculoPi.java
@@ -2,10 +2,25 @@
 
 public class calculoPi {
     public static void main(String[] args) {
-        
+        pi impar = new pi(1);
+        pi par = new pi(2);
     }
 }
 
 class pi extends Thread{
-    
+    int i;
+    static int op=0;
+    static double Value=0;
+    pi(int num){
+        this.i=num;
+    }
+    public void rest(){
+        yield();
+    }
+    public void run(){
+        int aux = i*2;
+        Value+=(aux/aux-1)*(aux/aux+1);
+        i++;
+        rest();
+    }
 }

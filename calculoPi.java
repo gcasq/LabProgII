071566a58efe187f3b59deeada1a41c95348e073
gcasq <gustavoacasqueiro@gmail.com>
2020-11-16 11:57:19 -0300

    Adição do esqueleto das classes do código

---
+++ calculoPi.java
@@ -0,0 +1,11 @@
+//Este exercício calculará o valor de Pi utilizando o recurso de Threads. O principal intuito é a prática da utilização deste recurso.
+
+public class calculoPi {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class pi extends Thread{
+    
+}

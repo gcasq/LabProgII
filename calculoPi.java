b37c829a2273104d14fe3cb9b49371d38330d755
gcasq <gustavoacasqueiro@gmail.com>
2020-11-16 12:42:51 -0300

    Exercício finalizado

--- calculoPi.java
+++ calculoPi.java
@@ -2,25 +2,34 @@
 
 public class calculoPi {
     public static void main(String[] args) {
-        pi impar = new pi(1);
-        pi par = new pi(2);
+        pi impar = new pi(1.0);
+        pi par = new pi(2.0);
+        impar.start();
+        par.start();
     }
 }
-
 class pi extends Thread{
-    int i;
+    double i;
     static int op=0;
-    static double Value=0;
-    pi(int num){
+    static double Value=1;
+    pi(double num){
         this.i=num;
     }
     public void rest(){
         yield();
     }
+    public void showvalue(){
+        System.out.println("Pi is equal to "+2.0*Value);
+    }
     public void run(){
-        int aux = i*2;
-        Value+=(aux/aux-1)*(aux/aux+1);
-        i++;
+        while(op<30000){
+        double aux = i*2;
+        Value = Value * (aux/(aux-1))*(aux/(aux+1));
+        i+=2;
+        op++;
         rest();
+        }
+        showvalue();
+
     }
 }

01907797ffd5709dd55b208aa3d17e8f569cdebc
gcasq <gustavoacasqueiro@gmail.com>
2020-12-04 17:32:28 -0300

    CRUD finalizado, com querys personalizados

--- mongo_python.py
+++ mongo_python.py
@@ -4,29 +4,30 @@ import json
 
 url = 'https://jsonplaceholder.typicode.com/users'
 params = {'limit': 16, 'country': 'us', 'apikey': 'API-KEY'}
-#response = requests.get(url, params=params,stream=True)
+response = requests.get(url, params=params,stream=True)
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["user_python"]
-mycol = mydb["teste"]
+mycol = mydb[""]
 
-#for i in response.json():
- #   x = mycol.insert_one(i)
-
-
+for i in response.json():
+    x = mycol.insert_one(i)
 
 def create():
-    print("Type the dictionary you want to insert")
+    print("Type the document you want to insert")
     dic_aux = input()
     json_acceptable_string = dic_aux.replace("'", "\"")
     try: 
         d = json.loads(json_acceptable_string)
-        x = mycol.insert_one(d)
-        print("document created with success, _id: ",x.inserted_id)          
+        try:
+            x = mycol.insert_one(d)
+            print("document created with success, _id: ",x.inserted_id)
+        except ValueError as e:
+            print('Failed. ',e)          
     except ValueError as e: 
-        print ("Not a valid JSON. Try again") 
+        print ("Not a valid dictionary. ",e) 
 
 def read():
-    print('type your query in dic format, empty curly brackets to read all: ')
+    print('Type your query in dictionary format, empty curly brackets to read all: ')
     query = input()
     json_acceptable_string = query.replace("'", "\"")
     try: 
@@ -34,43 +35,53 @@ def read():
         for x in mycol.find(query_dic):
             print(x)
     except ValueError as e: 
-        print ("Not a valid JSON. Try again")
+        print ("Not a valid dictionary. ",e)
 
 def update():
-    print("Select the id of the user you want to update: ")
-    id=int(input())
-    for x in mycol.find({'id':id}):
-        print(x)
-    myquery = { "id": int(id) }
-    print("Digite o dicionário que quer substituir: ")
-    print("Para campos aninhados, coloque \'field1.field2\':\'new value\'")
-    dic_aux = input()
-    json_acceptable_string = dic_aux.replace("'", "\"")
-    d = json.loads(json_acceptable_string)
-    newvalues = { "$set": d }
-    print("Update all? (y/n)")
-    answer = input()
-    if answer == 'y':
-        x = mycol.update_many(myquery, newvalues)
-        print(x.modified_count, "documents updated.")
-        return
+    print("Select the documents you want to update, in query dictionary format: ")
+    query = input()
+    json_acceptable_string = query.replace("'", "\"")
+    try: 
+        myquery = json.loads(json_acceptable_string)
+        print("Type the new keys and values to update, in dictionary format. New keys will generate new items")
+        print("To update nested values, type \'key1.key2\':\'new value\'")
+        dic_aux = input()
+        json_acceptable_string = dic_aux.replace("'", "\"")
+        try:
+            d = json.loads(json_acceptable_string)
+            newvalues = { "$set": d }
+            print("Update all? (y/n)")
+            answer = input()
+            if answer == 'y':
+                x = mycol.update_many(myquery, newvalues)
+                print(x.modified_count, "documents updated.")
+                return
 
-    x = mycol.update_one(myquery, newvalues)
-    print(x.modified_count, "documents updated.")
+            x = mycol.update_one(myquery, newvalues)
+            print(x.modified_count, "documents updated.")
+        except ValueError as e:
+            print("not a valid dictionary. ",e)
+
+    except ValueError as e: 
+        print ("Not a valid dictionary. ",e)
 
 def delete():
-    print("Select the id of the user you want to delete: ")
-    id = input()
-    
-    print("Delete all? (y/n)")
-    answer = input()
-    if answer == 'y':
-        x = mycol.delete_many({"id": int(id)})
-        print(x.deleted_count, " documents deleted.") 
-        return
-    
-    x = mycol.delete_one({"id": int(id)})
-    print(x.deleted_count, " documents deleted.") 
+    print("Select the documents you want to delete, in query dictionary format: ")
+    query = input()
+    json_acceptable_string = query.replace("'", "\"")
+    try: 
+        d = json.loads(json_acceptable_string)
+        print("Delete all? (y/n)")
+        answer = input()
+        if answer == 'y':
+            x = mycol.delete_many(d)
+            print(x.deleted_count, " documents deleted.") 
+            return
+        
+        x = mycol.delete_one(d)
+        print(x.deleted_count, " document deleted.")
+    except ValueError as e:
+        print("Not a valid dictionary format. ",e)
         
 while True:
     print("Select your action: ")
